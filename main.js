8c82feef9efd18c9a58da94dd97e6cac5a826f77
Bekzod <88338975+Hardskil@users.noreply.github.com>
2022-10-13 13:19:51 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,126 @@
+document.querySelector(`#btn`).addEventListener('click',function(e) {
+    document.querySelector(`#sec_m`).style.display = 'block'
+    document.querySelector(`#btn_2`).style.display = 'block'
+    document.querySelector(`#btn`).style.display = 'none'
+})
+document.querySelector('#btn_2').addEventListener('click',function(e){
+    document.querySelector(`#sec_m`).style.display = 'none'
+    document.querySelector(`#btn_2`).style.display = 'none'
+    document.querySelector(`#btn`).style.display = 'block'
+})
+
+
+document.querySelector(`#btn-2`).addEventListener('click',function(e) {
+    document.querySelector(`#sec_m-2`).style.display = 'block'
+    document.querySelector(`#btn_2-2`).style.display = 'block'
+    document.querySelector(`#btn-2`).style.display = 'none'
+})
+document.querySelector('#btn_2-2').addEventListener('click',function(e){
+    document.querySelector(`#sec_m-2`).style.display = 'none'
+    document.querySelector(`#btn_2-2`).style.display = 'none'
+    document.querySelector(`#btn-2`).style.display = 'block'
+})
+
+
+document.querySelector(`#btn-3`).addEventListener('click',function(e) {
+    document.querySelector(`#sec_m-3`).style.display = 'block'
+    document.querySelector(`#btn_2-3`).style.display = 'block'
+    document.querySelector(`#btn-3`).style.display = 'none'
+})
+document.querySelector('#btn_2-3').addEventListener('click',function(e){
+    document.querySelector(`#sec_m-3`).style.display = 'none'
+    document.querySelector(`#btn_2-3`).style.display = 'none'
+    document.querySelector(`#btn-3`).style.display = 'block'
+})
+
+
+
+document.querySelector(`#btn-4`).addEventListener('click',function(e) {
+    document.querySelector(`#sec_m-4`).style.display = 'block'
+    document.querySelector(`#btn_2-4`).style.display = 'block'
+    document.querySelector(`#btn-4`).style.display = 'none'
+})
+document.querySelector('#btn_2-4').addEventListener('click',function(e){
+    document.querySelector(`#sec_m-4`).style.display = 'none'
+    document.querySelector(`#btn_2-4`).style.display = 'none'
+    document.querySelector(`#btn-4`).style.display = 'block'
+})
+
+
+document.querySelector(`#btn-5`).addEventListener('click',function(e) {
+    document.querySelector(`#sec_m-5`).style.display = 'block'
+    document.querySelector(`#btn_2-5`).style.display = 'block'
+    document.querySelector(`#btn-5`).style.display = 'none'
+})
+document.querySelector('#btn_2-5').addEventListener('click',function(e){
+    document.querySelector(`#sec_m-5`).style.display = 'none'
+    document.querySelector(`#btn_2-5`).style.display = 'none'
+    document.querySelector(`#btn-5`).style.display = 'block'
+})
+
+
+
+
+document.querySelector(`#btn-6`).addEventListener('click',function(e) {
+    document.querySelector(`#sec_m-6`).style.display = 'block'
+    document.querySelector(`#btn_2-6`).style.display = 'block'
+    document.querySelector(`#btn-6`).style.display = 'none'
+})
+document.querySelector('#btn_2-6').addEventListener('click',function(e){
+    document.querySelector(`#sec_m-6`).style.display = 'none'
+    document.querySelector(`#btn_2-6`).style.display = 'none'
+    document.querySelector(`#btn-6`).style.display = 'block'
+})
+
+
+document.querySelector(`#btn-7`).addEventListener('click',function(e) {
+    document.querySelector(`#sec_m-7`).style.display = 'block'
+    document.querySelector(`#btn_2-7`).style.display = 'block'
+    document.querySelector(`#btn-7`).style.display = 'none'
+})
+document.querySelector('#btn_2-7').addEventListener('click',function(e){
+    document.querySelector(`#sec_m-7`).style.display = 'none'
+    document.querySelector(`#btn_2-7`).style.display = 'none'
+    document.querySelector(`#btn-7`).style.display = 'block'
+})
+
+
+
+
+document.querySelector(`#btn-8`).addEventListener('click',function(e) {
+    document.querySelector(`#sec_m-8`).style.display = 'block'
+    document.querySelector(`#btn_2-8`).style.display = 'block'
+    document.querySelector(`#btn-8`).style.display = 'none'
+})
+document.querySelector('#btn_2-8').addEventListener('click',function(e){
+    document.querySelector(`#sec_m-8`).style.display = 'none'
+    document.querySelector(`#btn_2-8`).style.display = 'none'
+    document.querySelector(`#btn-8`).style.display = 'block'
+})
+
+
+
+
+document.querySelector(`#btn-9`).addEventListener('click',function(e) {
+    document.querySelector(`#sec_m-9`).style.display = 'block'
+    document.querySelector(`#btn_2-9`).style.display = 'block'
+    document.querySelector(`#btn-9`).style.display = 'none'
+})
+document.querySelector('#btn_2-9').addEventListener('click',function(e){
+    document.querySelector(`#sec_m-9`).style.display = 'none'
+    document.querySelector(`#btn_2-9`).style.display = 'none'
+    document.querySelector(`#btn-9`).style.display = 'block'
+})
+
+
+
+document.querySelector(`#btn-10`).addEventListener('click',function(e) {
+    document.querySelector(`#sec_m-10`).style.display = 'block'
+    document.querySelector(`#btn_2-10`).style.display = 'block'
+    document.querySelector(`#btn-10`).style.display = 'none'
+})
+document.querySelector('#btn_2-10').addEventListener('click',function(e){
+    document.querySelector(`#sec_m-10`).style.display = 'none'
+    document.querySelector(`#btn_2-10`).style.display = 'none'
+    document.querySelector(`#btn-10`).style.display = 'block'
+})
